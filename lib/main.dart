e01306dca74e2cc8ce6434cba98393d209b0b01b
patchareeporn-saratee <saratee_p@silpakorn.edu>
2025-02-09 17:28:42 +0700

    Update main.dart
    homework2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:week3lab/page_answer/answer_portal.dart';
-import 'package:week3lab/page_answer/answer1.dart';
-import 'package:week3lab/page_answer/answer2.dart';
-import 'package:week3lab/page_answer/answer3.dart';
-import 'package:week3lab/page_answer/answer4.dart';
+import '../Homework2/air_quality.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -14,25 +11,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
-      routes: {HomePage.routeName: (context) => const HomePage()},
-      initialRoute: HomePage.routeName,
-    );
-  }
-}
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const HomePage());
+        home:   AQI ());
   }
+}
+
+  
+  
